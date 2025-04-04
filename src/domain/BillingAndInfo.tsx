d4b19b22fdfb398234ce13d526c2e4e118a0701b
Renan092992 <heineaas0904@gmail.com>
2025-04-04 11:46:48 -0700

    domainPage

--- src/domain/BillingAndInfo.tsx
+++ src/domain/BillingAndInfo.tsx
@@ -45,9 +45,9 @@ const DomainRegistration: React.FC = () => {
 
       {/* Main content */}
       <div className="w-[100%] h-[calc(100%-104px)] flex justify-center items-center ">
-        <div className="relative w-full max-w-[600px] min-w-[360px] h-[640px] ">
+        <div className="relative w-full min-w-[400px] min-w-[360px] h-[640px] ">
           {/* White card container */}
-          <div className=" bg-slate-400/40 p-[10px] rounded-[30px]">
+          <div className=" bg-slate-400 p-[10px] rounded-[30px]">
             <div className="w-full bg-white rounded-[24px] p-[32px] shadow-xl">
               {step === 'form' && (
                 <div className="space-y-[24px]">
